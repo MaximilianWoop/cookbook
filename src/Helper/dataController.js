9dbb94d270bf191e94c581d6e3a586e4526f48c7
MaximilianWoop-Dev <67760090+MaximilianWoop-Dev@users.noreply.github.com>
2021-04-27 08:44:05 +0200

    commit

--- src/Helper/dataController.js
+++ src/Helper/dataController.js
@@ -1,7 +1,7 @@
 //CRUD functions for recipe
 async function createRecipe(recipe){
     var request = new XMLHttpRequest();
-    request.open('POST','http://cookbook.ryotecx.de/api.php/recipe');
+    request.open('POST','https://cookbook.ryotecx.de/api.php/recipe');
     request.setRequestHeader("Content-Type", "application/json");
     var recipeJson = JSON.stringify(recipe);
     request.send(recipeJson); 
@@ -12,7 +12,7 @@ async function updateRecipe(recipe){
 }
 async function deleteRecipe(id){
     var request = new XMLHttpRequest();
-    request.open('DELETE','http://cookbook.ryotecx.de/api.php/recipe',true);
+    request.open('DELETE','https://cookbook.ryotecx.de/api.php/recipe',true);
     request.setRequestHeader("Content-Type", "application/json");
     var recipeJson = "{\"recipeID\":" + id + "}";
     console.log(recipeJson);
@@ -21,7 +21,7 @@ async function deleteRecipe(id){
 //CRUD functions for ingredient
 async function createIngredient(Ingredient){
     var request = new XMLHttpRequest();
-    request.open('POST','http://cookbook.ryotecx.de/api.php/ingredient');
+    request.open('POST','https://cookbook.ryotecx.de/api.php/ingredient');
     request.setRequestHeader("Content-Type", "application/json");
     var ingredientJson = JSON.stringify(Ingredient);
     console.log(ingredientJson);
@@ -38,21 +38,21 @@ async function deleteIngredient(id){
 
 //Get data 
 async function getRecipesFromURL(ref){
-    return await fetch('http://cookbook.ryotecx.de/api.php/recipe')
+    return await fetch('https://cookbook.ryotecx.de/api.php/recipe')
         .then(response => response.json())
         .then(data => data.forEach((recipe) => {  
             ref.$store.dispatch('setRecipe',recipe);
         }));
 }
 async function getIngredientsFromURL(ref){
-    return await fetch('http://cookbook.ryotecx.de/api.php/ingredient')
+    return await fetch('https://cookbook.ryotecx.de/api.php/ingredient')
         .then(response => response.json())
         .then(data => data.forEach((ingredient) => {  
             ref.$store.dispatch('setIngredient',ingredient);
         }));
 }
 async function getTagsFromURL(ref){
-    return await fetch('http://cookbook.ryotecx.de/api.php/tag')
+    return await fetch('https://cookbook.ryotecx.de/api.php/tag')
         .then(response => response.json())
         .then(data => data.forEach((tag) => {  
             ref.$store.dispatch('setTag',tag);
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+const fs = require('fs')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    https: true,
+    public:'https://localhost:8080/',
+    clientLogLevel: 'silent',
+    proxy: 'http://localhost:4000'
+  },
 }
+
+
+// {
+  
+//   keys: fs.readFileSync("./certs/key.pem"),
+//   cert: fs.readFileSync("./certs/cert.pem"),
+// },
